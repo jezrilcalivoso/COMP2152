076aa298d02306bfb7217051534311df338a5afc
jezrilcalivoso <jezril.calivoso@georgebrown.ca>
2025-01-14 14:55:29 -0500

    Lab 1 Coding Questions

---
+++ Week01/Lab/Coding Questions/lab1.py
@@ -0,0 +1,18 @@
+# Sample Coding Questions 01 Week 01
+# First Name: Jezril
+# Last Name: Calivoso
+
+arr = [1, 4, 7, 9]
+
+a = 1
+b = 2
+c = 3
+d = 4
+e = a - ((b ** c) // d) + (a % c)
+
+temperature = 32.6
+print(f"The temperature today is: {format(temperature, '.3f')} degrees Celsius")
+
+userAge = int(input("Enter your age: "))
+userAge += 22
+print(f"Now showing the shop items filtered by age: {userAge}")
